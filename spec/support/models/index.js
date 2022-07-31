b752fd33d81a00857c60d3e563cb076db2cbddd1
Wyatt Greenway <th317erd@gmail.com>
2022-07-31 10:35:13 -0700

    Completed adding model hooks

--- spec/support/models/index.js
+++ spec/support/models/index.js
@@ -1,14 +1,15 @@
 'use strict';
 
-const ExtendedUser  = require('./extended-user-model');
-const Number        = require('./number-model');
-const Role          = require('./role-model');
-const RoleThing     = require('./role-thing-model');
-const ScopedUser    = require('./scoped-user-model');
-const User          = require('./user-model');
-const UserRole      = require('./user-role-model');
-const UserThing     = require('./user-thing-model');
-const Time          = require('./time-model');
+const ExtendedUser    = require('./extended-user-model');
+const Number          = require('./number-model');
+const Role            = require('./role-model');
+const RoleThing       = require('./role-thing-model');
+const ScopedUser      = require('./scoped-user-model');
+const User            = require('./user-model');
+const UserRole        = require('./user-role-model');
+const UserThing       = require('./user-thing-model');
+const ValidationTest  = require('./validation-test-model');
+const Time            = require('./time-model');
 
 module.exports = {
   ExtendedUser,
@@ -19,5 +20,6 @@ module.exports = {
   User,
   UserRole,
   UserThing,
+  ValidationTest,
   Time,
 };
---
+++ spec/support/models/validation-test-model.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const { Model, Types } = require('../../../lib');
+
+class ValidationTest extends Model {
+  static fields = {
+    'id': {
+      type:         Types.XID,
+      defaultValue: Types.XID.Default.XID,
+      allowNull:    false,
+      primaryKey:   true,
+    },
+    'number': {
+      type:         Types.STRING,
+      index:        true,
+      validate:     async (value) => {
+        if (!(/^\d+$/).test(value))
+          throw new Error('Number expected');
+      },
+    },
+    'boolean': {
+      type:         Types.STRING,
+      index:        true,
+      validate:     async (value) => {
+        if (!(/^(true|false)$/).test(value))
+          throw new Error('Boolean expected');
+      },
+    },
+    'date': {
+      type:         Types.STRING,
+      index:        true,
+      validate:     async (value) => {
+        if (!(/^\d{4}-\d{2}-\d{2}$/).test(value))
+          throw new Error('Invalid date');
+      },
+    },
+  };
+}
+
+module.exports = ValidationTest;
