137f4f05272d2e412c3bd8a1de8596efe40bdf6e
Wyatt Greenway <th317erd@gmail.com>
2022-11-16 22:49:13 -0700

    WIP GROUP BY, query engine field merging

--- lib/utils/query-utils.d.ts
+++ lib/utils/query-utils.d.ts
@@ -4,8 +4,15 @@ import { Model, ModelClass } from '../model';
 import { QueryEngine } from '../query-engine';
 
 export declare function parseFilterFieldAndOperator(fieldName: string): { field: string, operator: string };
+
 export declare function generateQueryFromFilter(
   connection: ConnectionBase,
   Model: ModelClass,
   filter: Array<GenericObject | Model> | GenericObject | Model,
 ): QueryEngine;
+
+export declare function margeFields(
+  connection: ConnectionBase,
+  currentFields: Map<string, any>,
+  incomingFields: Array<any>
+): Map<string, any>;
--- spec/utils/query-utils-spec.js
+++ spec/utils/query-utils-spec.js
@@ -7,6 +7,25 @@
 const { Utils, ConnectionBase } = require('../../lib');
 
 describe('Utils::query', () => {
+  let connection;
+  let User;
+  let Role;
+
+  beforeAll(async () => {
+    try {
+      connection = new ConnectionBase({
+        bindModels: false,
+        models:     require('../support/models'),
+      });
+
+      let models = connection.getModels();
+      User = models.User;
+      Role = models.Role;
+    } catch (error) {
+      console.error('Error in beforeAll: ', error);
+    }
+  });
+
   describe('parseFilterFieldAndOperator', function() {
     it('should be able to parse a field', function() {
       expect(Utils.parseFilterFieldAndOperator(' test ')).toEqual({
@@ -77,29 +96,79 @@ describe('Utils::query', () => {
   });
 
   describe('generateQueryFromFilter', function() {
-    let connection;
-    let User;
-
-    beforeAll(async () => {
-      try {
-        connection = new ConnectionBase({
-          bindModels: false,
-          models:     require('../support/models'),
-        });
-
-        let models = connection.getModels();
-        User = models.User;
-      } catch (error) {
-        console.error('Error in beforeAll: ', error);
-      }
-    });
-
     it('should throw an error on a bad operator for an array value', function() {
       expect(() => Utils.generateQueryFromFilter(connection, User, { 'lastName>': [ 'derp' ] })).toThrow(new Error('Invalid array value for operator ">"'));
     });
 
     it('should throw an error on a bad operator for a NULL value', function() {
       expect(() => Utils.generateQueryFromFilter(connection, User, { 'firstName>': null })).toThrow(new Error('Invalid "NULL" value for operator ">"'));
+    });
+  });
+
+  describe('margeFields', function() {
+    it('should be able to merge fields from scratch', function() {
+      let result = Utils.margeFields(User, connection, undefined, [ 'firstName', 'lastName' ]);
+      expect(result.size).toEqual(2);
+      expect(result.get('User:firstName')).toBe(connection.getField('firstName', 'User'));
+      expect(result.get('User:lastName')).toBe(connection.getField('lastName', 'User'));
+    });
+
+    it('should be able to merge fields', function() {
+      // Start from blank
+      let result = Utils.margeFields(User, connection, undefined, [ 'firstName', 'lastName' ]);
+      expect(result.size).toEqual(2);
+      expect(result.get('User:firstName')).toBe(connection.getField('firstName', 'User'));
+      expect(result.get('User:lastName')).toBe(connection.getField('lastName', 'User'));
+
+      // Add
+      result = Utils.margeFields(User, connection, result, [ '+', 'primaryRoleID' ]);
+      expect(result.size).toEqual(3);
+      expect(result.get('User:firstName')).toBe(connection.getField('firstName', 'User'));
+      expect(result.get('User:lastName')).toBe(connection.getField('lastName', 'User'));
+      expect(result.get('User:primaryRoleID')).toBe(connection.getField('primaryRoleID', 'User'));
+
+      // Add plus re-add
+      result = Utils.margeFields(User, connection, result, [ '+', User.fields.firstName, '+lastName' ]);
+      expect(result.size).toEqual(3);
+      expect(result.get('User:firstName')).toBe(connection.getField('firstName', 'User'));
+      expect(result.get('User:lastName')).toBe(connection.getField('lastName', 'User'));
+      expect(result.get('User:primaryRoleID')).toBe(connection.getField('primaryRoleID', 'User'));
+
+      // Subtract
+      result = Utils.margeFields(User, connection, result, [ '-', User.fields.primaryRoleID ]);
+      expect(result.size).toEqual(2);
+      expect(result.get('User:firstName')).toBe(connection.getField('firstName', 'User'));
+      expect(result.get('User:lastName')).toBe(connection.getField('lastName', 'User'));
+
+      // Mixed add and subtract
+      result = Utils.margeFields(User, connection, result, [ '-firstName', '+', User.fields.primaryRoleID ]);
+      expect(result.size).toEqual(2);
+      expect(result.get('User:lastName')).toBe(connection.getField('lastName', 'User'));
+      expect(result.get('User:primaryRoleID')).toBe(connection.getField('primaryRoleID', 'User'));
+
+      // Reset with model
+      result = Utils.margeFields(User, connection, result, [ Role ]);
+      expect(result.size).toEqual(2);
+      expect(result.get('Role:id')).toBe(connection.getField('id', 'Role'));
+      expect(result.get('Role:name')).toBe(connection.getField('name', 'Role'));
+
+      // Add with model
+      result = Utils.margeFields(User, connection, result, [ '+User' ]);
+      expect(result.size).toEqual(6);
+      expect(result.get('Role:id')).toBe(connection.getField('id', 'Role'));
+      expect(result.get('Role:name')).toBe(connection.getField('name', 'Role'));
+      expect(result.get('User:id')).toBe(connection.getField('id', 'User'));
+      expect(result.get('User:firstName')).toBe(connection.getField('firstName', 'User'));
+      expect(result.get('User:lastName')).toBe(connection.getField('lastName', 'User'));
+      expect(result.get('User:primaryRoleID')).toBe(connection.getField('primaryRoleID', 'User'));
+
+      // Subtract field by name
+      result = Utils.margeFields(User, connection, result, [ '-User:firstName' ]);
+      expect(result.size).toEqual(5);
+
+      // Subtract model by name
+      result = Utils.margeFields(User, connection, result, [ '-Role' ]);
+      expect(result.size).toEqual(3);
     });
   });
 });
