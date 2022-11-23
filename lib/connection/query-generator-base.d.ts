c830a55e8fcb5e6c5eab071a2dcb2ebbb0c35882
Wyatt Greenway <th317erd@gmail.com>
2022-11-22 17:12:48 -0700

    Remove SQL specific code

--- lib/connection/query-generator-base.d.ts
+++ lib/connection/query-generator-base.d.ts
@@ -1,70 +1,9 @@
 import Field from '../field';
 import { GenericObject } from '../interfaces/common';
-import { ModelClass } from '../model';
 import { QueryEngine } from '../query-engine';
 import ConnectionBase from './connection-base';
 import { AverageLiteral, CountLiteral, DistinctLiteral, FieldLiteral, MaxLiteral, MinLiteral, SumLiteral } from './literals';
 import LiteralBase from './literals/literal-base';
-
-export declare interface GetEscapedFieldNameOptions {
-  fieldNameOnly?: boolean;
-}
-
-export declare interface GetEscapedTableNameNameOptions {
-  tableNamePrefix?: string;
-}
-
-export declare interface GetEscapedColumnNameOptions extends GetEscapedTableNameNameOptions {
-  columnNamePrefix?: string
-  columnNameOnly?: boolean;
-}
-
-export declare interface GetEscapedProjectionNameOptions extends GetEscapedColumnNameOptions, GetEscapedFieldNameOptions {
-  noProjectionAliases?: boolean;
-}
-
-export declare interface GetEscapedModelFieldsOptions extends GetEscapedProjectionNameOptions {
-  asProjection?: boolean;
-  asColumn?: boolean;
-}
-
-export declare interface ProjectedFieldInfo {
-  projectedName: string;
-  Model: ModelClass;
-  modelName: string;
-  Field: Field;
-  fieldName: string;
-  direction: string;
-  fullFieldName: string;
-}
-
-export declare interface JoinTableInfo {
-  operator: string;
-  joinType: string | LiteralBase;
-  rootModelName: string;
-  joinModel: ModelClass;
-  joinModelName: string;
-  leftSideModel: ModelClass;
-  leftSideModelName: string;
-  leftQueryContext: GenericObject;
-  leftSideField: Field;
-  rightSideModel: ModelClass;
-  rightSideModelName: string;
-  rightQueryContext: GenericObject;
-  rightSideField: Field;
-}
-
-export declare interface FieldDirectionInfo {
-  hasDirection: boolean;
-  direction: string;
-  fieldName: string;
-}
-
-export declare interface FieldOrderInfo {
-  Model: ModelClass;
-  Field: Field;
-  direction: string;
-}
 
 declare class QueryGeneratorBase {
   public constructor(connection);
@@ -72,26 +11,8 @@ declare class QueryGeneratorBase {
   public setOptionsCache(options: GenericObject, keyPath: string, value: any): void;
   public escape(field: Field, value: any, options?: GenericObject): string;
   public escapeID(value: LiteralBase | string, options?: GenericObject): string;
-  public getEscapedFieldName(Model: ModelClass | null | undefined, field: Field, options?: GetEscapedFieldNameOptions): string;
-  public getEscapedColumnName(Model: ModelClass | null | undefined, field: Field, options?: GetEscapedColumnNameOptions): string;
-  public getEscapedTableName(modelOrField: ModelClass | Field, options?: GetEscapedTableNameNameOptions): string;
-  public getEscapedProjectionName(Model: ModelClass | null | undefined, field: Field, options?: GetEscapedProjectionNameOptions): string;
-  public getEscapedModelFields(Model: ModelClass, options?: GetEscapedModelFieldsOptions): { [key: string]: string };
-  public isFieldIdentifier(value: string): boolean;
-  public getProjectedFields(queryEngine: QueryEngine, options?: GenericObject, asMap?: false | undefined): Array<string>;
-  public getProjectedFields(queryEngine: QueryEngine, options?: GenericObject, asMap?: true): Map<string, string>;
 
-  public getJoinTableInfoFromQueryContexts(
-    leftQueryContext: GenericObject,
-    rightQueryContext: GenericObject,
-    joinType: string | LiteralBase,
-    options?: GenericObject
-  ): JoinTableInfo;
-
-  public getFieldDirectionSpecifier(order: LiteralBase): LiteralBase;
-  public getFieldDirectionSpecifier(order: string | Field): FieldDirectionInfo;
-
-  public getQuerySliceFromQueryPart(queryPart: GenericObject): Array<GenericObject>;
+  public _getLiteralAlias(literal: LiteralBase, options?: GenericObject);
   public _averageLiteralToString(literal: AverageLiteral, options?: GenericObject): string;
   public _countLiteralToString(literal: CountLiteral, options?: GenericObject): string;
   public _distinctLiteralToString(literal: DistinctLiteral, options?: GenericObject): string;
